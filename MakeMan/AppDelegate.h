5fcdbad6fa522c757b8efb1631b0f4daf8a10f77
Dickordia <dickordia@gmail.com>
2013-01-10 22:57:57 +0200

    Initial Commit

---
+++ MakeMan/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MakeMan
+//
+//  Created by Sergey Sergeivich on 10.01.13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
